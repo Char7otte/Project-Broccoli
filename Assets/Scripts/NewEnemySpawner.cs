8405c8606730ce36b135946877b1a0818be2eb7f
shar <faethychan@gmail.com>
2023-07-04 00:47:14 +0800

    Project Files

---
+++ Assets/Scripts/NewEnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewEnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    
+    public float time_to_spawn_enemy;
+    private float timer;
+
+    private void Update()
+    {
+        if (instance.maximum_enemy_count > 0) SpawnEnemyTimer();
+    }
+
+    private void SpawnEnemyTimer()
+    {
+        timer += Time.deltaTime;
+        if (timer >= time_to_spawn_enemy) SpawnEnemy();
+    }
+
+    private void SpawnEnemy() 
+    {
+        Instantiate(EnemyPrefab, transform.position, Quaternion.identity, gameObject.transform);
+        timer = 0;
+
+        instance.maximum_enemy_count--;
+    }
+}
---
+++ Assets/Scripts/NewGameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGameManager : MonoBehaviour
+{
+    public static NewGameManager instance;
+    public int maximum_enemy_count;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
+    
+}
