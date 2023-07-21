91271767b6b152e33539fb6f44482f51f556eb71
shar <faethychan@gmail.com>
2023-07-21 10:48:38 +0800

    Enemy detection added

--- Assets/Scripts/Characters/Enemy/EnemyController.cs
+++ Assets/Scripts/Characters/Enemy/EnemyController.cs
@@ -9,16 +9,28 @@ public class EnemyController : MonoBehaviour
 
     private Transform player;
     private NavMeshAgent navMeshAgent;
+    private bool playerDetected = false;
 
     private void Start () {
         player = GameObject.Find("Player").transform;
         navMeshAgent = GetComponent<NavMeshAgent>();
         navMeshAgent.speed = movementSpeed;
     }
+
+    private void Update() {
+        transform.LookAt(player);
+    }
 	
 	private void FixedUpdate () {
         if (player == null) return;
+        if (!playerDetected) return;
 
         navMeshAgent.SetDestination(player.position);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            playerDetected = true;
+        }
     }
 }
