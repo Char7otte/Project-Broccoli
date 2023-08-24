075d0433b7e0b8bc6608b48955066e4da1e6a111
shar <faethychan@gmail.com>
2023-08-25 03:30:27 +0800

    Boss room added

---
+++ Assets/Scripts/Level Managers/Level 2/ActivateDoor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateDoor : MonoBehaviour
+{
+    [SerializeField]private GameObject door;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            door.SetActive(true);
+        }
+    }
+}
